cad35c0829dcb96f58cd151955e943817bacf2f9
Ian SooHoo <ian@iansoohoo.me>
2022-06-27 03:51:41 -0700

    refactor efficiency

--- colorgenerator/main.js
+++ colorgenerator/main.js
@@ -10,36 +10,28 @@ ctx.stroke();
 updateDownload();
 
 function updateDownload(){
-    let cnt3 = document.getElementById("toAdd");
+    let download_button = document.getElementById("dloadbutton");
     const img = cnvs.toDataURL('image/png');
-    let btn = document.createElement("a");
-    btn.classList.add("button");
-    btn.href = img;
-    btn.download = "color.png";
-    btn.text = "Downlad Image";
-    if(cnt3.children.length>1){
-        cnt3.removeChild(cnt3.children[1]);
-    }
-    cnt3.appendChild(btn);
+    download_button.href = img;
 }
 
 
-// function changecolor() {
+function changecolor() {
+    updateDownload();
     
 //     ctx.fillStyle = pick.value; 
 //     // console.log("color is now");
 //     // console.log(pick.value);
 
-// }
+}
 
 
-document.addEventListener('click', function(event) {
-    let isClickInsideElement = pick.contains(event.target);
-    if (!isClickInsideElement) {
+pick.addEventListener('input', function(event) {
+   
         // console.log("WOWW")
         ctx.fillStyle = pick.value;
         ctx.fillRect(0,0,1920,1080);
         ctx.stroke();
         updateDownload();
-    }
+    
 });
